ca2af40ce37433263c7c5a048fba0c3b87b155af
A. Thompson Sr <tbone@MacBookPro.attlocal.net>
2025-08-13 23:15:25 -0500

    trying to get pretty diagnostics

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -2,7 +2,13 @@ require('ts-error-translator').setup {
   auto_override_publish_diagnostics = true,
 }
 require('tsc').setup { use_diagnostics = true, auto_start_watch_mode = true }
-require('typescript-tools').setup {}
+local translate = require('ts-error-translator').translate_diagnostics
+require('typescript-tools').setup {
+  handlers = {
+    ['textDocument/publishDiagnostics'] = function(err, result, ctx)
+      translate(err, result, ctx)
+      vim.lsp.diagnostic.on_publish_diagnostics(err, result, ctx)
+    end,
+  },
+}
 
--- Enable inlay hints for TypeScript
-vim.lsp.inlay_hint.enable(true)
