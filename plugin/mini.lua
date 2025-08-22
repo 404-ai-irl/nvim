75eaa07eda95f6a416ca0486616e4b265338acb1
Andrew Thompson <andrew@taltytech.com>
2025-08-22 11:34:03 -0500

    updated mini.move keybinds

--- plugin/mini.lua
+++ plugin/mini.lua
@@ -1,4 +1,15 @@
-require('mini.move').setup()
+require('mini.move').setup {
+  mappings = {
+    left = '<C-S-h>',
+    right = '<C-S-l>',
+    down = '<C-S-j>',
+    up = '<C-S-k>',
+    line_left = '<C-S-h>',
+    line_right = '<C-S-l>',
+    line_down = '<C-S-j>',
+    line_up = '<C-S-k>',
+  },
+}
 require('mini.ai').setup()
 require('mini.surround').setup()
 require('mini.comment').setup()
