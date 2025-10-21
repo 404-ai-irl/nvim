6345fad07ad4b50e4dc5003e98b8c8a6e2ea72b1
Andrew Thompson <andrew@taltytech.com>
2025-10-21 07:46:30 -0500

    colors ..

--- lua/404-ai-irl/colors.lua
+++ lua/404-ai-irl/colors.lua
@@ -1,1 +1,16 @@
-vim.pack.add 'https://github.com/EdenEast/nightfox.nvim'
+local nf = require 'nightfox'
+nf.setup {
+  options = {
+    options = {
+      colorblind = {
+        enable = true,
+        severity = {
+          protan = 0.3,
+          deutan = 0.6,
+        },
+      },
+    },
+  },
+}
+nf.compile()
+vim.cmd 'colorscheme carbonfox'
