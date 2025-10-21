119e14f23ed7ae112996e8c091e267bf5c46cc94
Andrew Thompson Sr <tbone@MacBookPro.attlocal.net>
2025-10-20 22:41:55 -0500

    update

--- lua/404-ai-irl/autocmd.lua
+++ lua/404-ai-irl/autocmd.lua
@@ -1,5 +1,13 @@
 --- :help lua-guide-autocommands
-
+-- Oil + snacks,rename
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'OilActionsPost',
+  callback = function(event)
+    if event.data.actions[1].type == 'move' then
+      Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+    end
+  end,
+})
 --- UI ---
 -- Highlight yanks
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -9,4 +17,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
-
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,68 +4,255 @@ return {
   lazy = false,
   keys = {
     -- Core picker operations
-    { '<leader><space>', function() require('snacks').picker.smart() end, desc = 'Smart Find Files' },
-    { '<leader>,', function() require('snacks').picker.buffers() end, desc = 'Buffers' },
-    { '<leader>/', function() require('snacks').picker.grep() end, desc = 'Grep' },
-    { '<leader>:', function() require('snacks').picker.command_history() end, desc = 'Command History' },
+    {
+      '<leader><space>',
+      function()
+        require('snacks').picker.smart()
+      end,
+      desc = 'Smart Find Files',
+    },
+    {
+      '<leader>,',
+      function()
+        require('snacks').picker.buffers()
+      end,
+      desc = 'Buffers',
+    },
+    {
+      '<leader>/',
+      function()
+        require('snacks').picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>:',
+      function()
+        require('snacks').picker.command_history()
+      end,
+      desc = 'Command History',
+    },
 
     -- Find operations
-    { '<leader>fb', function() require('snacks').picker.buffers() end, desc = 'Buffers' },
-    { '<leader>fc', function() require('snacks').picker.files({ cwd = vim.fn.stdpath('config') }) end, desc = 'Find Config File' },
-    { '<leader>ff', function() require('snacks').picker.files() end, desc = 'Find Files' },
-    { '<leader>fg', function() require('snacks').picker.git_files() end, desc = 'Find Git Files' },
-    { '<leader>fr', function() require('snacks').picker.recent() end, desc = 'Recent Files' },
+    {
+      '<leader>fb',
+      function()
+        require('snacks').picker.buffers()
+      end,
+      desc = 'Buffers',
+    },
+    {
+      '<leader>fc',
+      function()
+        require('snacks').picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = 'Find Config File',
+    },
+    {
+      '<leader>ff',
+      function()
+        require('snacks').picker.files()
+      end,
+      desc = 'Find Files',
+    },
+    {
+      '<leader>fg',
+      function()
+        require('snacks').picker.git_files()
+      end,
+      desc = 'Find Git Files',
+    },
+    {
+      '<leader>fr',
+      function()
+        require('snacks').picker.recent()
+      end,
+      desc = 'Recent Files',
+    },
 
     -- Git operations
-    { '<leader>gb', function() require('snacks').picker.git_branches() end, desc = 'Git Branches' },
-    { '<leader>gl', function() require('snacks').picker.git_log() end, desc = 'Git Log' },
-    { '<leader>gs', function() require('snacks').picker.git_status() end, desc = 'Git Status' },
+    {
+      '<leader>gb',
+      function()
+        require('snacks').picker.git_branches()
+      end,
+      desc = 'Git Branches',
+    },
+    {
+      '<leader>gl',
+      function()
+        require('snacks').picker.git_log()
+      end,
+      desc = 'Git Log',
+    },
+    {
+      '<leader>gs',
+      function()
+        require('snacks').picker.git_status()
+      end,
+      desc = 'Git Status',
+    },
 
     -- Search operations
-    { '<leader>sb', function() require('snacks').picker.lines() end, desc = 'Buffer Lines' },
-    { '<leader>sg', function() require('snacks').picker.grep() end, desc = 'Grep' },
-    { '<leader>sw', function() require('snacks').picker.grep_word() end, desc = 'Visual selection or word', mode = { 'n', 'x' } },
-    { '<leader>sc', function() require('snacks').picker.command_history() end, desc = 'Command History' },
-    { '<leader>sC', function() require('snacks').picker.commands() end, desc = 'Commands' },
-    { '<leader>sd', function() require('snacks').picker.diagnostics() end, desc = 'Diagnostics' },
-    { '<leader>sh', function() require('snacks').picker.help() end, desc = 'Help Pages' },
-    { '<leader>sk', function() require('snacks').picker.keymaps() end, desc = 'Keymaps' },
-    { '<leader>sR', function() require('snacks').picker.resume() end, desc = 'Resume' },
+    {
+      '<leader>sb',
+      function()
+        require('snacks').picker.lines()
+      end,
+      desc = 'Buffer Lines',
+    },
+    {
+      '<leader>sg',
+      function()
+        require('snacks').picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>sw',
+      function()
+        require('snacks').picker.grep_word()
+      end,
+      desc = 'Visual selection or word',
+      mode = { 'n', 'x' },
+    },
+    {
+      '<leader>sc',
+      function()
+        require('snacks').picker.command_history()
+      end,
+      desc = 'Command History',
+    },
+    {
+      '<leader>sC',
+      function()
+        require('snacks').picker.commands()
+      end,
+      desc = 'Commands',
+    },
+    {
+      '<leader>sd',
+      function()
+        require('snacks').picker.diagnostics()
+      end,
+      desc = 'Diagnostics',
+    },
+    {
+      '<leader>sh',
+      function()
+        require('snacks').picker.help()
+      end,
+      desc = 'Help Pages',
+    },
+    {
+      '<leader>sk',
+      function()
+        require('snacks').picker.keymaps()
+      end,
+      desc = 'Keymaps',
+    },
+    {
+      '<leader>sR',
+      function()
+        require('snacks').picker.resume()
+      end,
+      desc = 'Resume',
+    },
 
     -- LSP operations
-    { 'gd', function() require('snacks').picker.lsp_definitions() end, desc = 'Goto Definition' },
-    { 'gr', function() require('snacks').picker.lsp_references() end, nowait = true, desc = 'References' },
-    { 'gI', function() require('snacks').picker.lsp_implementations() end, desc = 'Goto Implementation' },
-    { 'gy', function() require('snacks').picker.lsp_type_definitions() end, desc = 'Goto Type Definition' },
-    { '<leader>ss', function() require('snacks').picker.lsp_symbols() end, desc = 'LSP Symbols' },
-    { '<leader>sS', function() require('snacks').picker.lsp_workspace_symbols() end, desc = 'LSP Workspace Symbols' },
+    {
+      'gd',
+      function()
+        require('snacks').picker.lsp_definitions()
+      end,
+      desc = 'Goto Definition',
+    },
+    {
+      'gr',
+      function()
+        require('snacks').picker.lsp_references()
+      end,
+      nowait = true,
+      desc = 'References',
+    },
+    {
+      'gI',
+      function()
+        require('snacks').picker.lsp_implementations()
+      end,
+      desc = 'Goto Implementation',
+    },
+    {
+      'gy',
+      function()
+        require('snacks').picker.lsp_type_definitions()
+      end,
+      desc = 'Goto Type Definition',
+    },
+    {
+      '<leader>ss',
+      function()
+        require('snacks').picker.lsp_symbols()
+      end,
+      desc = 'LSP Symbols',
+    },
+    {
+      '<leader>sS',
+      function()
+        require('snacks').picker.lsp_workspace_symbols()
+      end,
+      desc = 'LSP Workspace Symbols',
+    },
   },
-  config = function()
-    require('snacks').setup({
-      picker = {
-        enabled = true,
-        layouts = {
-          telescope = {
-            reverse = true,
-            layout = {
-              box = "horizontal",
-              backdrop = false,
-              width = 0.8,
-              height = 0.9,
-              border = "none",
-              {
-                box = "vertical",
-                { win = "list", title = " Results ", title_pos = "center", border = "single" },
-                { win = "input", height = 1, border = "single", title = "{title} {live} {flags}", title_pos = "center" },
-              },
-              { win = "preview", title = "{preview:Preview}", width = 0.45, border = "single", title_pos = "center" },
-            },
-          },
-        },
-        layout = "telescope",
+  opts = {
+    -- Available layout presets: bottom, default, dropdown, ivy, ivy_split, left, right, select, sidebar, telescope, top, vertical, vscode
+    picker = { 
+      enabled = true,
+      layout = {
+        preset = "ivy_split",
       },
-      profiler = { enabled = true },
-      dashboard = { enabled = true },
-    })
-  end,
+      layouts = {
+        ivy_split = {
+          layout = {
+            border = "rounded",
+          }
+        }
+      }
+    },
+    profiler = { enabled = true },
+    dashboard = { 
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { section = "keys", gap = 1, padding = 1 },
+        { section = "startup" },
+      },
+      preset = {
+        header = [[
+ _   _                      
+| |_| |__   ___  _ __   ___ 
+| __| '_ \ / _ \| '_ \ / _ \
+| |_| |_) | (_) | | | |  __/
+ \__|_.__/ \___/|_| |_|\___|
+                           
+]],
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua require('snacks').picker.files()" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua require('snacks').picker.recent()" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua require('snacks').picker.grep()" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua require('snacks').picker.files({cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Store", action = function() vim.cmd("Lazy") end },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+    rename = { enabled = true },
+    bigfile = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
 }
+
